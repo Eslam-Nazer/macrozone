d47d648846966dcaa68c292b7657a67d6047e4d0
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-06-08 23:21:35 +0300

    Make pages as tabs

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,37 +1,40 @@
-import { colors } from "@/styles/global";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.header,
-        },
-        headerTintColor: "#fff",
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          title: "Home",
-        }}
-      />
+    // <Stack
+    //   screenOptions={{
+    //     headerStyle: {
+    //       backgroundColor: colors.header,
+    //     },
+    //     headerTintColor: "#fff",
+    //   }}
+    // >
+    //   <Stack.Screen
+    //     name="index"
+    //     options={{
+    //       headerShown: false,
+    //       title: "Home",
+    //     }}
+    //   />
 
-      <Stack.Screen
-        name="meals"
-        options={{
-          title: "Meals",
-        }}
-      />
+    //   <Stack.Screen
+    //     name="meals"
+    //     options={{
+    //       title: "Meals",
+    //     }}
+    //   />
 
-      <Stack.Screen
-        options={{
-          title: "Add Meal",
-        }}
-        name="add-meal"
-      />
+    //   <Stack.Screen
+    //     options={{
+    //       title: "Add Meal",
+    //     }}
+    //     name="add-meal"
+    //   />
+    // </Stack>
+
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
