fc97b31849d968ee1beef3774f3d238ec5dd9f7b
Eslam-Nazer <eslam.nazer43@gmail.com>
2026-06-10 11:08:03 +0300

    Bind meals to macro grid and change to dynamice data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -29,7 +29,7 @@ export default function HomeScreen() {
     >
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <MacroGrid />
+      <MacroGrid meals={meals} />
       <RecentMealsScreen meals={meals} />
     </ScrollView>
   );
